89185bc9447973a57506b8441c80ff291390c77a
Masashi Omuro <masashi0660@gmail.com>
2023-03-15 23:09:33 +0900

    First Commit

---
+++ HalloWorld/src/Sample/Hallo.java
@@ -0,0 +1,10 @@
+package Sample;
+
+public class Hallo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HalloWorld/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author masashi0660
+ *
+ */
+module HalloWorld {
+}
